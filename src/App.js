d17db4df74c5ea922378a04ce6dbec30468c496b
vinodhleo <vinodhleo@outlook.com>
2022-03-30 10:02:28 +0530

    first-commit-today

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
 import Square from './Square'
+import {useState} from 'react'
 
  const App =()=>{
+   let [vegetables,setVegetables]=useState(["Onion","Potato","Pumpkin"])
+   let [fruits,setFruits]=useState(["Oranges","Grapes","Apple"])
+   let [spices,setSpices]=useState(["Pepper","Cinnamon","Cardamom"])
+   let [electronics,setElectronics]=useState(["Pendrive","Mobile","Tablet"])
+   let [cosmetics,setCosmetics]=useState(["Powder","Cream"])
    return(
         <div>
                <h1>First component</h1>
-               <Square title="Vegetables"/>
-               <Square title="Fruits"/>
-               <Square title="Spices"/>
-               <Square title="Electronics"/>
-               <Square title="Cosmetics"/>
+               <Square title="Vegetables" items={vegetables} />
+               <Square title="Fruits" items = {fruits}/>
+               <Square title="Spices" items = {spices}/>
+               <Square title="Electronics" items = {electronics}/>
+               <Square title="Cosmetics" items = {cosmetics}/>
         </div>
    )
  }
--- src/Square.js
+++ src/Square.js
@@ -1,9 +1,13 @@
 
 
-const Square =({title})=>{
+const Square =({title,items})=>{
     return(
     <div className="square">
              <h3>  {title} </h3>
+             <hr/>
+             <ul>
+                 {items.map(x=> <li key={x}>{x}</li>)}
+             </ul>
     </div>
     )
 }
