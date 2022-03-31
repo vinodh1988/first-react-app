82ad108062793761c42cc9ab16739929a8728de8
vinodhleo <vinodhleo@outlook.com>
2022-03-31 11:13:02 +0530

    first-commit

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,9 +1,27 @@
+import axios from "axios"
+import { useState ,useEffect} from "react"
+import ContactBox from "./ContactBox"
 
-const Contact = ()=>{
-    return (
-        <h1>Contact us</h1>
+const Contact=()=>{
+    useEffect(async ()=>{
+        
+        try{
+            let result=await axios.get("http://localhost:4500/contacts")
+            console.log(result)
+            setContacts(result.data)
+           }
+       catch(e){
+
+       }
+    },[])
+    const [contacts,setContacts]=useState([])
+    return(
+    <div>
+        {
+            contacts.map(x=><ContactBox key={x.title} contact={x}></ContactBox>)
+        }
+     </div>
     )
 }
-
 
 export default Contact
